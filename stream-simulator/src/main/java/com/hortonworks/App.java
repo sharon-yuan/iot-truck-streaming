85c0ed862cab0506efbe3309aa6ef52980404a62
sharon-yuan <wangshouyuan@aliyun.com>
2016-08-25 15:23:21 +0800

    iot-eclipse

--- stream-simulator/src/main/java/com/hortonworks/App.java
+++ stream-simulator/src/main/java/com/hortonworks/App.java
@@ -8,27 +8,27 @@ import akka.actor.UntypedActorFactory;
 
 import com.hortonworks.streaming.impl.domain.transport.TruckConfiguration;
 import com.hortonworks.streaming.impl.messages.StartSimulation;
-import com.hortonworks.streaming.impl.messages.StopSimulation;
 import com.hortonworks.streaming.listeners.SimulatorListener;
 import com.hortonworks.streaming.masters.SimulationMaster;
 
+@SuppressWarnings("deprecation")
 public class App {
-  public static void main(String[] args) {
+  public static void main(String[] args1) {
 
     final String DEFAULT_EMITTER = "com.hortonworks.streaming.impl.domain.transport.Truck";
     final String DEFAULT_COLLECTOR= "com.hortonworks.streaming.impl.collectors.KafkaEventCollector";
-
+String[] args={ "6" ,"250" ,"com.hortonworks.streaming.impl.domain.transport.Truck", "com.hortonworks.streaming.impl.collectors.StdOutEventCollector" ,"1000" ,"/etc/storm_demo/routes/midwest","1000"};
 
     try {
 
-      final Class eventEmitterClass = args[2] == null ?
+      final Class<? extends Object> eventEmitterClass = args[2] == null ?
           Class.forName(DEFAULT_EMITTER) : Class.forName(args[2]);
-      final Class eventCollectorClass = args[3] == null ?
+      final Class<? extends Object> eventCollectorClass = args[3] == null ?
           Class.forName(DEFAULT_COLLECTOR) : Class.forName(args[3]);
       final long demoId = Long.parseLong(args[4]);
       final String routesDirectory = args[5];
       final Integer messageDelay = new Integer(args[6]);
-
+System.out.println("routesDirectory:"+routesDirectory);
       TruckConfiguration.initialize(routesDirectory);
       final int numberOfEventEmitters = TruckConfiguration.freeRoutePool.size();
 
@@ -46,9 +46,13 @@ public class App {
       System.out.println(eventCollector.path());
 
 
-      final ActorRef master = system.actorOf(new Props(
+
+	final ActorRef master = system.actorOf(new Props(
           new UntypedActorFactory() {
-            public UntypedActor create() {
+          
+			private static final long serialVersionUID = -3621148210901309380L;
+
+			public UntypedActor create() {
               return new SimulationMaster(
                   numberOfEventEmitters,
                   eventEmitterClass, listener, numberOfEvents, demoId,
